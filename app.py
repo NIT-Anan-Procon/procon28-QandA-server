4449c113e749048f57d235d6fe8b6080a29444d9
kohki <tk011875@gmail.com>
2017-10-01 01:48:25 +0900

    add: str json encode dict

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import unicode_literals
+from argparse import ArgumentParser
+from flask import Flask, request, render_template, abort, send_from_directory
+import json
 
 import os
 import sys
-from argparse import ArgumentParser
-
-from flask import Flask, request, abort, send_from_directory, render_template
-from flask import Flask, render_template, request
-from datetime import datetime
-import json
-from datetime import timedelta
 import time
 import requests
-import json
+from datetime import datetime
+from datetime import timedelta
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     #return 'hogehoge'
     return render_template('index.html', message="moririn dayo")
 
+
 @app.route("/callback", methods=['GET'])
 def callback():
 
     return 'aaaaaa'
+
 
 @app.route("/get", methods=['GET'])
 def get():
@@ -41,9 +41,29 @@ def get():
 
     return str_out
 
+
 @app.route("/post", methods=['POST'])
 def post_back():
+    # $ curl -i -H "Content-Type: application/json" -XST -d '{"key":"val","id":10}' http://127.0.0.1:8000/post
+    print('form', request.form)
+    print('args', request.args)
+    print('values', request.values)
+    print('cookies', request.cookies)
+    print('stream', request.stream)
+    print('headers', request.headers)
+    print('data', request.data)
+    print('type(data)', type(request.data))
+    print('method)', request.method)
+    print('get_json', request.get_json)
+    print('is_json', request.is_json)
 
+    #bytes -> str(json) -> dict
+    bytes_data = request.data  # bytes配列
+    str_data = bytes_data.decode('utf-8')  # 文字列に変換
+    #print('str_data', type(str_data), str_data)
+    json_data = json.loads(str_data)
+
+    '''
     message = []
     message.append("*** post_proc *** start ***")
     message.append("request.method = " + request.method)
@@ -53,6 +73,9 @@ def post_back():
 
     str_out = json.dumps(message)    
     return str_out
+    '''
+    return json_data['key']
+
 
 if __name__ == "__main__":
     arg_parser = ArgumentParser(
