e97c85b4d7dad42f0cac3f2f16202e2c0a476063
skriulle <skriulle5@gmail.com>
2018-01-18 15:17:19 +0900

    add:beep sound

--- app.py
+++ app.py
@@ -43,26 +43,6 @@ password = ""
 NAMESPACE_MAP = "/map"
 NAMESPACE_INTERVIEW = "/input"
 
-"""
-if NAME == 'localQAserver':
-    host = "localhost"
-    port = 5432
-    dbname = "qadb"
-    user = "tomoya"
-    password = ""
-elif NAME == 'herokuQAserver':
-    host = "ec2-54-83-3-101.compute-1.amazonaws.com"
-    port = 5432
-    dbname = "d5s9osbhq5v6sn"
-    user = "bpnislhqjpweyk"
-    password = "7735ffd9623f5372ad5e8db15cd70bedfc7a9c9edbc033f1b21c419e4f4a1e02"
-
-
-
-print("connect DB")
-print("host="+host+" port="+str(port)+" dbname="+dbname+" user="+user+" password="+password+"")
-connection = psycopg2.connect("host="+host+" port="+str(port)+" dbname="+dbname+" user="+user+" password="+password+"")
-"""
 connection = getDatabaseConnection()
 connection.get_backend_pid()
 cur = connection.cursor()
